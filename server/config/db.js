0568a5006f8225661f224374f6a93976075ef7bd
21Errors <kamogeloerickolobe@gmail.com>
2025-07-29 09:04:12 +0200

    Fix spelling erorr

--- server/config/db.js
+++ server/config/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'sql7.freesqldatabase.com',
   user: 'sql7792361',        // XAMPP MySQL user
-  database: 'ssql7792361',
+  database: 'sql7792361',
   password: 'vmURWUAyDc',        // If no password
   port: 3306,          // If you configured XAMPP MySQL to use this port
 });
