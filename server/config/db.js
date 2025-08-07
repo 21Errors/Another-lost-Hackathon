df86dc613450e86efb9cf6f0ca0086fddc431442
21Errors <kamogeloerickolobe@gmail.com>
2025-08-07 14:15:42 +0200

    Database changes

--- server/config/db.js
+++ server/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'sql7.freesqldatabase.com',
-  user: 'sql7792361',        // XAMPP MySQL user
-  database: 'sql7792361',
-  password: 'vmURWUAyDc',        // If no password
+  host: 'sql8.freesqldatabase.com',
+  user: 'sql8794014',        // XAMPP MySQL user
+  database: 'sql8794014',
+  password: 'S2HR6DNsHU',        // If no password
   port: 3306,          // If you configured XAMPP MySQL to use this port
 });
 
